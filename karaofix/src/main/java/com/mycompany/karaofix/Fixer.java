99a16f0f2ad01228b6b955a0625d7bcf4afc0257
José Antonio Parejo Maestre <japarejo@gmail.com>
2017-10-26 14:08:01 +0200

    Añadida clse principal

---
+++ karaofix/src/main/java/com/mycompany/karaofix/Fixer.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.karaofix;
+
+/**
+ *
+ * @author japar
+ */
+public class Fixer {
+    public static void main(String[] args) {
+        // TODO: Completar el Ejercicio aquí. :-D
+    }
+}
